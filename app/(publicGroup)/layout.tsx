ace448fb64c6572a07829ddc4c7c7b642eb27289
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-08-01 17:09:05 +0600

    navbar created

---
+++ app/(publicGroup)/layout.tsx
@@ -0,0 +1,18 @@
+import { Navbar } from "@/components/shared/Navbar";
+import { getMe } from "@/services/getMe";
+
+const PublicGroupLayout = async ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const user = await getMe();
+  return (
+    <div>
+      <Navbar user={user} />
+      {children}
+    </div>
+  );
+};
+
+export default PublicGroupLayout;
